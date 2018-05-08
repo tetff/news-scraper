3845f276b905a1e7bacbb8c7ebcd0c11b3c9a262
Tóth Máté <mate.toth@flowacademy.hu>
2018-05-08 15:35:41 +0200

    Api Post method added

--- api/api.go
+++ api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"github.com/tothmate90/news-scraper/newsapi"
 	"encoding/json"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/olivere/elastic"
@@ -16,11 +19,12 @@ import (
 )
 
 type Handler struct {
-	Mux *chi.Mux
-	ElasticHandler  *elasticsearch.Handler
+	Mux            *chi.Mux
+	ElasticHandler elasticsearch.Handler
+	values         url.Values
 }
 
-func New(port string, eH *elasticsearch.Handler) Handler {
+func New(port string, eH elasticsearch.Handler) Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.RequestID)
 	mux.Use(middleware.RealIP)
@@ -30,8 +34,8 @@ func New(port string, eH *elasticsearch.Handler) Handler {
 	mux.Use(render.SetContentType(render.ContentTypeJSON))
 	http.ListenAndServe(":"+port, mux)
 	return Handler{
-		Mux: mux,
-		ElasticHandler:  eH,
+		Mux:            mux,
+		ElasticHandler: eH,
 	}
 }
 
@@ -47,8 +51,18 @@ func (h *Handler) Get() {
 	})
 }
 
-func (h *Handler) GetAll(from, size int) {
+func (h *Handler) GetAll() {
 	h.Mux.Get("/news-api/v1/", func(w http.ResponseWriter, r *http.Request) {
+		from, err := strconv.Atoi(h.values.Get("from"))
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		size, err := strconv.Atoi(h.values.Get("size"))
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
 		sR, err := h.ElasticHandler.GetAll(from, size)
 		if err != nil {
 			w.WriteHeader(400)
@@ -58,21 +72,45 @@ func (h *Handler) GetAll(from, size int) {
 	})
 }
 
-func (h *Handler) Post(country, category string) {
+func (h *Handler) Post() {
 	h.Mux.Post("/news-api/v1/", func(w http.ResponseWriter, r *http.Request) {
-		
-	})
-}
+		values := url.Values{}
+		values.Add("country", h.values.Get("country"))
+		values.Add("category", h.values.Get("category"))
 
-func articleResponse(sR *elastic.SearchResult, w http.ResponseWriter) {
-	articles := utils.Translator(sR)
-		message, err := json.Marshal(articles)
+		result, err := newsapi.GetTopHeadlines(values)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		err = h.ElasticHandler.Post(result.Articles)
+		if err != nil {
+			w.WriteHeader(400)
+			return			
+		}
+		message, err := json.Marshal(result.Articles)
 		if err != nil {
 			w.WriteHeader(400)
 			return
 		}
 		_, err = w.Write(message)
 		if err != nil {
-			return
+			w.WriteHeader(400)
+			return			
 		}
+	})
+}
+
+func articleResponse(sR *elastic.SearchResult, w http.ResponseWriter) {
+	articles := utils.Translator(sR)
+	message, err := json.Marshal(articles)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+	_, err = w.Write(message)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
 }
